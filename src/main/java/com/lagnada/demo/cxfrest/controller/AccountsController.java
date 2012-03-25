6c0cbbb033cebb1373ef00392bb402f76e104db6
antonio@lagnada.com <antonio@lagnada.com@88664369-b00a-0410-895a-a014b692d462>
2012-03-25 04:51:44 +0000

    fixed java version in mvn compiler
    git-svn-id: http://svn.lagnada.com/repos/dev/Sandbox/cxf-rest/trunk@1895 88664369-b00a-0410-895a-a014b692d462

--- src/main/java/com/lagnada/demo/cxfrest/controller/AccountsController.java
+++ src/main/java/com/lagnada/demo/cxfrest/controller/AccountsController.java
@@ -7,6 +7,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.lang.reflect.Method;
+import java.net.URI;
 
 @Produces({"application/json", "application/xml"})
 @Consumes({"application/json", "application/xml"})
@@ -16,7 +19,21 @@ public class AccountsController {
     @POST
     @Path("/accounts")
     public Response getAccount(Account account) {
-        return Response.ok(account).build();
+        Method getAccount = getMethod(AccountController.class, "getAccount", String.class);
+        URI newAccountUri = UriBuilder.fromResource(AccountController.class)
+                .path(getAccount)
+                .build(1);
+        return Response.created(newAccountUri)
+                .entity(account)
+                .build();
+    }
+
+    private Method getMethod(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        try {
+            return clazz.getMethod(methodName, parameterTypes);
+        } catch (Exception e) {
+            throw (new RuntimeException(e));
+        }
     }
 
 }
